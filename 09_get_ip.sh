615efd0b83fae55a5554b49e1ddc784f82f96424
Quentin JOLY <github@thoughtless.eu>
2024-03-12 11:05:01 +0100

    Add IP files to talos-scripts directory

--- 09_get_ip.sh
+++ 09_get_ip.sh
@@ -5,10 +5,13 @@
 json_data=$(openstack server list -f json)
 admin_ip=$(echo "$json_data" | jq -r '.[] | select(.Name == "admin-machine") | .Networks."ext-net1"[0]')
 echo -e "Admin IP: \n$admin_ip"
+echo "$admin_ip" > ./talos-scripts/admin_ip.txt
 echo "-------------------"
 control_plane_ips=$(echo "$json_data" | jq 'sort_by(.Name)' | jq -r '.[] | select(.Name | startswith("control-plane")) | .Networks."talos-net"[0]')
 echo -e "Control Plane IPs: \n$control_plane_ips"
+echo "$control_plane_ips" > ./talos-scripts/control_plane_ips.txt
 echo "-------------------"
 worker_ips=$(echo "$json_data" | jq 'sort_by(.Name)' | jq -r '.[] | select(.Name | startswith("worker")) | .Networks."talos-net"[0]')
 
 echo -e "Worker IPs: \n$worker_ips"
+echo "$worker_ips" > ./talos-scripts/worker_ips.txt
