0ef4174abfa0d840b06e1a752d59ec3610595f7f
Quentin JOLY <github@thoughtless.eu>
2024-03-12 11:14:48 +0100

    Add installation scripts for kubectl, talosctl, and talhelper

---
+++ talos-scripts/01_install_utils.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install kubectl
+curl -LO https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+chmod 555 ./kubectl
+mv kubectl /usr/local/bin/kubectl
+
+# Install talosctl
+curl -sL https://talos.dev/install | sh
+
+# Install talhelper
+curl https://i.jpillora.com/budimanjojo/talhelper! | sudo bash
